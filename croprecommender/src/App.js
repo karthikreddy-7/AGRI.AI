832d5e52b9d81fbaf021604835cb1c3c63aad700
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-07 11:28:37 +0530

    tailwind

--- croprecommender/src/App.js
+++ croprecommender/src/App.js
@@ -16,13 +16,13 @@ function App() {
               "Content-Type": "application/json",
             },
             body: JSON.stringify({
-              N: 40,
-              P: 40,
-              K: 40,
-              temperature: 30,
-              humidity: 20,
-              ph: 20,
-              rainfall: 100,
+              N: 10,
+              P: 10,
+              K: 10,
+              temperature: 20,
+              humidity: 10,
+              ph: 5,
+              rainfall: 1,
             }),
           }
         );
@@ -36,24 +36,7 @@ function App() {
 
     postData();
   }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1 className="text-xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
