c1c0d026322c32d1824f4bbf2639847dda2f51c9
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-07 09:27:17 +0530

    frontend

--- croprecommender/src/App.js
+++ croprecommender/src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 function App() {
+  useEffect(() => {
+    const fetchApiData = async () => {
+      try {
+        // Fetch data from the API using the host URL from your local.env file
+        const response = await fetch(process.env.REACT_APP_API_HOST);
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchApiData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
